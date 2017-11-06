1d1d3bda8cd2577ecfbb406c2e91d78b581853e5
Rivno <abienne@gmail.com>
2017-11-06 17:18:30 +0100

    Update IndexViewModel.js

--- app/public/scripts/viewmodels/IndexViewModel.js
+++ app/public/scripts/viewmodels/IndexViewModel.js
@@ -6,22 +6,22 @@
 
 var isMobile = {
     Android: function () {
-        return navigator.userAgent.match(/Android/i) || false;
+        return !!navigator.userAgent.match(/Android/i);
     },
     BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i) || false;
+        return !!navigator.userAgent.match(/BlackBerry/i);
     },
     iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i) || false;
+        return !!navigator.userAgent.match(/iPhone|iPad|iPod/i);
     },
     Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i) || false;
+        return !!navigator.userAgent.match(/Opera Mini/i);
     },
     Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i) || false;
+        return !!navigator.userAgent.match(/IEMobile/i);
     },
     any: function () {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        return !!(isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
     }
 };
 
