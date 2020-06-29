82893857b1b2ed1116c54b2ade403b6762f862f0
Rivno <abienne@gmail.com>
2020-06-29 20:42:37 +0200

    Update feedController.js

--- app/api/feedController.js
+++ app/api/feedController.js
@@ -12,72 +12,150 @@ if (!String.prototype.startsWith) {
 
 exports.controller = function() {
     this.name = "feed";
+    
+    this.moduleList = [
+        {
+            Url: "http://feeds.feedburner.com/jster",
+            request: {
+                protocol: 'http',
+                host: 'feeds.feedburner.com',
+                path: '/jster',
+                headers: {
+                    'Content-Type': 'application/xml',
+                },
+            },
+            Column: 1,
+            Row: 2
+        },
+        {
+            Url: "https://mangadex.org/rss/follows/2YUk6ENMzvxsTu8HPBRXe4QZbStym9K3",
+            request: {
+                protocol: 'https',
+                host: 'mangadex.org',
+                path: '/rss/follows/2YUk6ENMzvxsTu8HPBRXe4QZbStym9K3',
+                headers: {
+                    'Content-Type': 'application/xml',
+                },
+            },
+            Column: 1,
+            Row: 1
+        },
+        {
+            Url: "http://www.horriblesubs.info/rss.php?res=1080",
+            request: {
+                protocol: 'http',
+                host: 'www.horriblesubs.info',
+                path: '/rss.php?res=1080',
+                headers: {
+                    'Content-Type': 'application/xml',
+                },
+            },
+            Column: 1,
+            Row: 3
+        },
+        {
+            Url: "http://www.jeuxvideo.com/rss/rss.xml",
+            request: {
+                protocol: 'http',
+                host: 'www.jeuxvideo.com',
+                path: '/rss/rss.xml',
+                headers: {
+                    'Content-Type': 'application/xml',
+                },
+            },
+            Column: 2,
+            Row: 1
+        },
+        {
+            Url: "https://www.gamekult.com/feed.xml",
+            request: {
+                protocol: 'https',
+                host: 'www.gamekult.com',
+                path: '/feed.xml',
+                headers: {
+                    'Content-Type': 'application/xml',
+                },
+            },
+            Column: 2,
+            Row: 2
+        },
+        {
+            Url: "https://www.gamespot.com/feeds/news/",
+            request: {
+                protocol: 'https',
+                host: 'www.gamespot.com',
+                path: '/feeds/news/',
+                headers: {
+                    'Content-Type': 'application/xml',
+                },
+            },
+            Column: 2,
+            Row: 3
+        },
+        {
+            Url: "https://www.theverge.com/rss/index.xml",
+            request: {
+                protocol: 'https',
+                host: 'www.theverge.com',
+                path: '/rss/index.xml',
+                headers: {
+                    'Content-Type': 'application/xml',
+                },
+            },
+            Column: 3,
+            Row: 1
+        },
+        {
+            Url: "https://www.lemonde.fr/rss/une.xml",
+            request: {
+                protocol: 'https',
+                host: 'www.lemonde.fr',
+                path: '/rss/une.xml',
+                headers: {
+                    'Content-Type': 'application/xml',
+                },
+            },
+            Column: 3,
+            Row: 2
+        },
+        {
+            Url: "http://rss.cnn.com/rss/edition_world.rss",
+            request: {
+                protocol: 'http',
+                host: 'rss.cnn.com',
+                path: '/rss/edition_world.rss',
+                headers: {
+                    'Content-Type': 'application/xml',
+                },
+            },
+            Column: 3,
+            Row: 3
+        }
+    ];
 
     this.listModule = function(req, res) {
-
-    	var data = {
-    		ListModule: [
-    			{
-    				Url: "http://feeds.feedburner.com/jster",
-    				Column: 1,
-    				Row: 2
-    			},
-    			{
-    				Url: "https://mangadex.org/rss/follows/2YUk6ENMzvxsTu8HPBRXe4QZbStym9K3",
-    				Column: 1,
-    				Row: 1
-    			},
-    			{
-    				Url: "http://www.horriblesubs.info/rss.php?res=1080",
-    				Column: 1,
-    				Row: 3
-    			},
-    			{
-    				Url: "http://www.jeuxvideo.com/rss/rss.xml",
-    				Column: 2,
-    				Row: 1
-    			},
-    			{
-    				Url: "https://www.gamekult.com/feed.xml",
-    				Column: 2,
-    				Row: 2
-    			},
-                {
-                    Url: "https://www.gamespot.com/feeds/news/",
-                    Column: 2,
-                    Row: 3
-                },
-    			{
-    				Url: "https://www.theverge.com/rss/index.xml",
-    				Column: 3,
-    				Row: 1
-    			},
-    			{
-    				Url: "https://www.lemonde.fr/rss/une.xml",
-    				Column: 3,
-    				Row: 2
-    			},
-                {
-                    Url: "http://rss.cnn.com/rss/edition_world.rss",
-                    Column: 3,
-                    Row: 3
-                }
-    		]};
-
-        res.json(data);
+        res.json({ ListModule: this.moduleList });
     };
 
     this.read = function (req, res) {
 		
 		var body = "";
+        let currentModule;
+        for (mod of this.moduleList) {
+            if (mod.Url === req.query.url) {
+                currentModule = mod;
+                break;
+            }
+        }
         
         var protocol = http;
         
-        if (req.query.url.startsWith("https")) {
+        //if (req.query.url.startsWith("https")) {
+        if (currentModule.request.protocol = 'https') {
             protocol = https;
         }
 
-		protocol.get( req.query.url, function (resp) {
+		protocol.get( currentModule.request, function (resp) {
 			resp.setEncoding("utf8");
 			resp.on('data', function (d) {
 				body += d;
