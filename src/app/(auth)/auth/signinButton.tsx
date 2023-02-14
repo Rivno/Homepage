7067439e985348b288d3bf143d87187efff63c9f
Allan Bienne <abienne@gmail.com>
2023-02-14 15:49:20 +0100

    feature - auth custom page

---
+++ src/app/(auth)/auth/signinButton.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { ClientSafeProvider, signIn } from 'next-auth/react';
+
+import LogoGithub from '../../../../public/cv/companies/github.svg';
+
+const logos = new Map();
+logos.set('github', LogoGithub);
+
+export function SignInButton({ provider }: { provider: ClientSafeProvider }) {
+  const Logo = logos.get(provider.name.toLowerCase());
+  console.log('logo', provider.name, Logo);
+
+  return (
+    <div key={provider.name}>
+      <button onClick={() => signIn(provider.id)}>
+        <span>
+          <Logo />
+        </span>
+        <span>Sign in with {provider.name}</span>
+      </button>
+    </div>
+  );
+}
